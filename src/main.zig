e620efe97d3d6d05138ca8941c45c6e50485fadb
Lukas Boelling <lukasboelling002@gmail.com>
2025-12-02 23:04:03 -0500

    Set up runner framework

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Instant = std.time.Instant;
+
 const aoc2025 = @import("aoc2025");
+const Solution= aoc2025.Solution;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -12,17 +16,78 @@ pub fn main() !void {
     }
     const allocator = gpa.allocator();
 
+    // Set up stdout
+    const stdout_buf = try allocator.alloc(u8, 1024);
+    defer allocator.free(stdout_buf);
+    var stdout = std.fs.File.writer(std.fs.File.stdout(), stdout_buf).interface;
+
+    // Directory with input files
     var input_dir = try std.fs.cwd().openDir("inputs/real/", .{});
     defer input_dir.close();
 
-    const file_1 = try input_dir.openFile("1", .{});
-    defer file_1.close();
+    // All solutions
+    const runners = [_]SolutionRunner{
+        solution_runner(u32, u32, aoc2025.solution_1),
+    };
 
-    const file_buf: []u8 = try allocator.alloc(u8, 1024 * 1024);
-    defer allocator.free(file_buf);
+    for (runners, 1..) |runner, day_n| {
+        const solution_info = try runner(allocator, input_dir, @as(u32, @intCast(day_n)));
+        defer solution_info.deinit(allocator);
 
-    var file_reader: std.fs.File.Reader = file_1.reader(file_buf);
-    const file_ioreader: *std.Io.Reader = &file_reader.interface;
-    const solution: aoc2025.Solution = try aoc2025.solution_1(file_ioreader, allocator);
-    std.debug.print("Day 1: {?d} {?d}\n", .{solution.part1, solution.part2});
+        try stdout.print("Day {d}, {d} ms:\n  Part 1: {s}\n  Part 2: {s}\n", .{day_n, solution_info.duration_ns / 1000, solution_info.part1, solution_info.part2});
+        try stdout.flush();
+    }
+}
+
+const SolutionInfo = struct {
+    duration_ns: u64,
+    part1: []u8,
+    part2: []u8,
+
+    pub fn deinit(self: SolutionInfo, allocator: Allocator) void {
+        allocator.free(self.part1);
+        allocator.free(self.part2);
+    }
+};
+
+const SolutionRunner = *const fn(allocator: Allocator, input_dir: std.fs.Dir, day_n: u32) anyerror!SolutionInfo;
+
+pub fn solution_runner(
+    comptime T: type, 
+    comptime U: type, 
+    solution: fn(Allocator, []u8) anyerror!Solution(T, U)
+) SolutionRunner {
+    const Runner = struct {
+        pub fn run(allocator: Allocator, input_dir: std.fs.Dir, day_n: u32) !SolutionInfo {
+            const filename = try std.fmt.allocPrint(allocator, "{d}", .{day_n});
+            defer allocator.free(filename);
+            
+            const file = try input_dir.openFile(filename, .{});
+            defer file.close();
+
+            const file_size: u64 = try file.getEndPos();
+            const file_buf: []u8 = try allocator.alloc(u8, file_size);
+            defer allocator.free(file_buf);
+
+            var file_reader: std.fs.File.Reader = file.reader(file_buf);
+            const file_ioreader: *std.Io.Reader = &file_reader.interface;
+
+            try file_ioreader.readSliceAll(file_buf);
+
+            const start_time = try Instant.now();
+            const sol = try solution(allocator, file_buf);
+            const end_time = try Instant.now();
+            const duration_ns = end_time.since(start_time);
+
+            const part1: []u8 = try std.fmt.allocPrint(allocator, "{any}", .{sol.part1});
+            const part2: []u8 = try std.fmt.allocPrint(allocator, "{any}", .{sol.part2});
+
+            return SolutionInfo {
+                .duration_ns = duration_ns,
+                .part1 = part1,
+                .part2 = part2,
+            };
+        }
+    };
+    return Runner.run;
 }
--- src/root.zig
+++ src/root.zig
@@ -1,51 +1,117 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-pub const Solution = struct {
-    part1: ?u32,
-    part2: ?u32,
+pub fn Solution(comptime T: type, comptime U: type) type {
+    return struct {
+        const Self = @This();
 
-    pub fn first_part(part1: u32) Solution {
-        return Solution {
-            .part1 = part1,
-            .part2 = null,
-        };
-    }
+        part1: ?T,
+        part2: ?U,
 
-    pub fn both_parts(part1: u32, part2: u32) Solution {
-        return Solution {
-            .part1 = part1,
-            .part2 = part2,
-        };
-    }
-};
+        pub fn first_part(part1: T) Self {
+            return Solution {
+                .part1 = part1,
+                .part2 = null,
+            };
+        }
 
-pub fn solution_1(reader: *std.Io.Reader, allocator: Allocator) !Solution {
-    const max_size = 1024 * 32;
-    const bytes: []u8 = try allocator.alloc(u8, max_size);
-    defer allocator.free(bytes);
-    const n_read = try reader.readSliceShort(bytes);
+        pub fn both_parts(part1: T, part2: U) Self {
+            return Self {
+                .part1 = part1,
+                .part2 = part2,
+            };
+        }
+    };
+}
 
-    if (reader.peek(1) != error.EndOfStream) {
-        return error.BufferTooSmall;
-    }
+pub fn solution_1(allocator: Allocator, bytes: []u8) !Solution(u32, u32) {
+    _ = allocator;
 
     var angle: u32 = 50;
     var zero_count: u32 = 0;
+    var p2_count: u32 = 0;
 
-    var lines = std.mem.splitScalar(u8, bytes[0..n_read], '\n');
+    var lines = std.mem.splitScalar(u8, bytes, '\n');
     while (lines.next()) |line| {
         if (line.len == 0) continue;
 
-        var val = try std.fmt.parseInt(u32, line[1..], 10);
-        val = val % 100;
+        const val = try std.fmt.parseInt(u32, line[1..], 10);
+
+        var right: bool = true;
         if (line[0] == 'L') {
-            val = 100 - val;
+            right = false;
         }
-       
-        angle = (angle + val) % 100;
+        const s = update_angle(angle, right, val);
+        angle = s.new_angle;
+        p2_count += s.num_zeros;
+
         if (angle == 0) zero_count += 1;
     }
 
-    return Solution.first_part(zero_count);
+    return Solution(u32, u32).both_parts(zero_count, p2_count);
+}
+
+fn update_angle(current_angle: u32, right: bool, val: u32) struct { new_angle: u32, num_zeros: u32 } {
+    var p2_count: u32 = 0;
+    var new_angle = current_angle;
+
+    p2_count += @intCast(@divFloor(val, 100));
+
+    if (right) {
+        new_angle = @mod(current_angle + val, 100);
+        if (current_angle > 0 and (new_angle < current_angle or new_angle == 0)) p2_count += 1;
+    } else {
+        const norm_val: u32 = 100 - @mod(val, 100);
+        new_angle = @mod(current_angle + norm_val, 100);
+        if (current_angle > 0 and (new_angle > current_angle or new_angle == 0)) p2_count += 1;
+    }
+
+    return .{
+        .new_angle = new_angle,
+        .num_zeros = p2_count,
+    };
+}
+
+const expectEqual = std.testing.expectEqual;
+
+test "update angle" {
+    try expectEqual(0, update_angle(0, true, 1).num_zeros);
+    try expectEqual(0, update_angle(0, true, 99).num_zeros);
+    try expectEqual(1, update_angle(0, true, 100).num_zeros);
+    try expectEqual(1, update_angle(0, true, 101).num_zeros);
+    try expectEqual(8, update_angle(0, false, 800).num_zeros);
+
+    try expectEqual(0, update_angle(5, true, 1).num_zeros);
+    try expectEqual(0, update_angle(5, true, 94).num_zeros);
+    try expectEqual(1, update_angle(5, true, 95).num_zeros);
+    try expectEqual(1, update_angle(5, true, 96).num_zeros);
+    try expectEqual(1, update_angle(5, true, 99).num_zeros);
+    try expectEqual(1, update_angle(5, true, 100).num_zeros);
+    try expectEqual(1, update_angle(5, true, 194).num_zeros);
+    try expectEqual(2, update_angle(5, true, 195).num_zeros);
+
+    try expectEqual(0, update_angle(0, false, 1).num_zeros);
+    try expectEqual(99, update_angle(0, false, 1).new_angle);
+    try expectEqual(0, update_angle(0, false, 99).num_zeros);
+    try expectEqual(1, update_angle(0, false, 100).num_zeros);
+    try expectEqual(1, update_angle(0, false, 101).num_zeros);
+
+    try expectEqual(0, update_angle(5, false, 1).num_zeros);
+    try expectEqual(1, update_angle(5, false, 5).num_zeros);
+    try expectEqual(1, update_angle(5, false, 99).num_zeros);
+    try expectEqual(1, update_angle(5, false, 100).num_zeros);
+    try expectEqual(1, update_angle(5, false, 104).num_zeros);
+    try expectEqual(2, update_angle(5, false, 105).num_zeros);
+    try expectEqual(2, update_angle(5, false, 195).num_zeros);
+
+    try expectEqual(10, update_angle(50, true, 1000).num_zeros);
+        
+    try expectEqual(8, update_angle(0, true, 823).num_zeros);
+
+    try expectEqual(1, update_angle(50, true, 50).num_zeros);
+    try expectEqual(0, update_angle(0, true, 50).num_zeros);
+    try expectEqual(1, update_angle(50, false, 50).num_zeros);
+    try expectEqual(0, update_angle(0, false, 50).num_zeros);
+    try expectEqual(1, update_angle(50, true, 75).num_zeros);
+    try expectEqual(1, update_angle(25, false, 50).num_zeros);
 }
